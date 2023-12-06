f44b736e11f347f99a84edc110f14c16eb004b6f
hp <engrziaurrehman.kicsit@gmail.com>
2023-12-06 23:11:54 +0500

    update

--- 02. Linear Regression/Project 1/ModelCreation.py
+++ 02. Linear Regression/Project 1/ModelCreation.py
@@ -4,11 +4,25 @@ import pickle as pk
 from sklearn import linear_model
 
 def createModel(data):
+
+    # Here, you are extracting the 'x' and 'y' columns from the DataFrame data. 
+    # In machine learning, 'x' typically represents the input features (independent variable), 
+    # and 'y' represents the target variable (dependent variable). 
     x = data['x']
     y = data['y']
 
-    x = np.array([x]).reshape(-1,1)
+    # After extracting the 'x' and 'y' columns, you convert them into NumPy arrays. This step is 
+    # important because many machine learning libraries, including scikit-learn (which is used in this script), 
+    # expect input data to be in the form of NumPy arrays or similar data structures.
+
+    x = np.array([x]).reshape(-1,1) # The -1 in the first dimension allows NumPy to automatically calculate the 
+                                    # number of rows needed to maintain all the elements from the original array.
     y = np.array([y]).reshape(-1,1)
+
+    # The reshape(-1, 1) part is used to reshape the 1-dimensional arrays into column vectors. In scikit-learn, 
+    # the input features (x) are expected to be a 2D array, where each row represents a sample, and each column 
+    # represents a feature. Reshaping ensures that your 'x' and 'y' arrays are formatted correctly for use with 
+    # scikit-learn's LinearRegression model.
     
     # train
     model = linear_model.LinearRegression()
